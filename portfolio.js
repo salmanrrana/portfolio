883f32557ff2c79e225f8bea12885b6cc7021ab2
Salman Rana <salman.r.rana@gmail.com>
2017-05-31 18:51:22 -0500

    more projects added, added modals and more descriptions to projects page

--- portfolio.js
+++ portfolio.js
@@ -28,4 +28,119 @@ $(function() {
   $('.contact-list').animate({
     opacity: 1
   }, {duration: 2000});
+
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  var modal2 = document.getElementById('myModal2');
+  var modal3 = document.getElementById('myModal3');
+  var modal4 = document.getElementById('myModal4');
+  var modal5 = document.getElementById('myModal5');
+  var modal6 = document.getElementById('myModal6');
+  var modal7 = document.getElementById('myModal7');
+
+
+
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById('myImg');
+  var img2 = document.getElementById('myImg2');
+  var img3 = document.getElementById('myImg3');
+  var img4 = document.getElementById('myImg4');
+  var img5 = document.getElementById('myImg5');
+  var img6 = document.getElementById('myImg6');
+  var img7 = document.getElementById('myImg7');
+
+
+
+
+
+  var modalImg = document.getElementById("img01");
+  var modalImg2 = document.getElementById("img02");
+  var modalImg3 = document.getElementById("img03");
+  var modalImg4 = document.getElementById("img04");
+  var modalImg5 = document.getElementById("img05");
+  var modalImg6 = document.getElementById("img06");
+  var modalImg7 = document.getElementById("img07");
+
+
+
+
+
+  img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+  }
+
+  img2.onclick = function(){
+      modal2.style.display = "block";
+      modalImg2.src = this.src;
+  }
+
+  img3.onclick = function(){
+      modal3.style.display = "block";
+      modalImg3.src = this.src;
+  }
+
+  img4.onclick = function(){
+      modal4.style.display = "block";
+      modalImg4.src = this.src;
+  }
+
+  img5.onclick = function(){
+      modal5.style.display = "block";
+      modalImg5.src = this.src;
+  }
+
+  img6.onclick = function(){
+      modal6.style.display = "block";
+      modalImg6.src = this.src;
+  }
+
+  img7.onclick = function(){
+      modal7.style.display = "block";
+      modalImg7.src = this.src;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  var span2 = document.getElementsByClassName("close2")[0];
+  var span3 = document.getElementsByClassName("close3")[0];
+  var span4 = document.getElementsByClassName("close4")[0];
+  var span5 = document.getElementsByClassName("close5")[0];
+  var span6 = document.getElementsByClassName("close6")[0];
+  var span7 = document.getElementsByClassName("close7")[0];
+
+
+
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  span2.onclick = function() {
+      modal2.style.display = "none";
+  }
+
+  span3.onclick = function() {
+      modal3.style.display = "none";
+  }
+
+  span4.onclick = function() {
+      modal4.style.display = "none";
+  }
+
+  span5.onclick = function() {
+      modal5.style.display = "none";
+  }
+
+  span6.onclick = function() {
+      modal6.style.display = "none";
+  }
+
+  span7.onclick = function() {
+      modal7.style.display = "none";
+  }
+
 });
